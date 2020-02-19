0141ea2ade46d5eed89ac7f0714c92c61f36c008
Nitheesh <nitheeshkr115@gmail.com>
2020-02-19 22:46:16 +0530

    bootstrap added

--- angular-okta/src/app/app.module.ts
+++ angular-okta/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'userlist', component: UserListComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent, UserListComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
